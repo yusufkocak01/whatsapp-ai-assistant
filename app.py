adbad8c42d8d3c75880123a4ce0d9a95d5420aa0
yusufkocak01 <ysfkck@gmail.com>
2025-11-25 20:53:08 +0300

    Update app.py

--- app.py
+++ app.py
@@ -83,5 +83,6 @@ def index():
     return "✅ Yusuf'un AI Asistanı (prompt.csv + ChatGPT)"
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 10000))
+    port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port)
+
